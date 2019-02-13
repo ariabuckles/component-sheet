a5d307a258e5afd3c82170879968e3e874f67a8b
Aria Buckles <aria@seesaw.me>
2019-02-12 17:15:40 -1000

    add DivWithTestID test to aphrodite

--- packages/aphrodite/tests/basic.test.js
+++ packages/aphrodite/tests/basic.test.js
@@ -1,7 +1,21 @@
+import * as React from 'react';
 import ComponentSheet from '@component-sheet/aphrodite';
 import { View } from '@khanacademy/wonder-blocks-core';
 import describeImplBasic from '@component-sheet/core/tests/basic.shared';
 import { flushToStyleTag } from 'aphrodite';
+
+class DivWithTestId extends React.Component {
+    render() {
+        return (
+            <div
+                className={this.props.className}
+                data-testid={this.props.testID}
+            >
+                {this.props.children}
+            </div>
+        );
+    }
+}
 
 describe('@component-sheet/aphrodite', () => {
     describeImplBasic(ComponentSheet, View, {
@@ -11,6 +25,12 @@ describe('@component-sheet/aphrodite', () => {
     });
 
     describeImplBasic(ComponentSheet, 'div', {
+        suiteName: 'div className',
+        styleKey: 'className',
+        afterRender: flushToStyleTag,
+    });
+
+    describeImplBasic(ComponentSheet, DivWithTestId, {
         suiteName: 'className prop',
         styleKey: 'className',
         afterRender: flushToStyleTag,
